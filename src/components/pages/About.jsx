28e4491582d7531dbce6f342c9bd56f334b010ec
Aldrien Hate <aldrienhate@yahoo.com>
2018-02-16 10:40:44 +0800

    Fixed simple typo.

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -28,7 +28,7 @@ class About extends Component {
         </h6>
 
         <br/>
-        <p>Developed at: February 9, 2018</p>
+        <p>Developed: February 9, 2018</p>
       </div>
     );
   }
